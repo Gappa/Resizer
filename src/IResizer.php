f3f9c1c6f00ea5c739fb4794b365318da61e3f5e
Pavel Linhart <pavel.linhart@gmail.com>
2020-02-11 08:15:30 +0100

    removed deprecated method

--- src/IResizer.php
+++ src/IResizer.php
@@ -7,17 +7,6 @@ use Nette\Utils\Html;
 
 interface IResizer
 {
-	/** @deprecated */
-	public function resize(
-		string $path,
-		string $irParams = null,
-		string $alt = null,
-		string $title = null,
-		string $class = null,
-		string $id = null,
-		bool $useAssets = false
-	): Html;
-
 	public function process(
 		string $path,
 		?string $params,
